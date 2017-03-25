c33a7c3083f2fa813bede63c21423ea48abb46ff
antonmry <anton@galiglobal.com>
2017-03-25 22:02:13 +0100

    Final version

--- src/java/ScriptGroovyExample.groovy
+++ src/java/ScriptGroovyExample.groovy
@@ -5,7 +5,7 @@ import groovyx.net.http.RESTClient
 def jokes = new RESTClient('http://tambal.azurewebsites.net')
 def resp = jokes.get( path: '/joke/random/' )
 
-assert resp.status == 200
+assert resp.status == 500
 
 println "Joke: " + resp.data.joke
 
